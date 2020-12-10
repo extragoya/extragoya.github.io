aff37b7b3178724b3b8b990496a2f20a6317edfd
Adam Harrison <adampharrison070@paii-labs.com>
2020-12-10 18:16:26 -0500

    Tryfixing date

--- markdown_generator/pubsFromBib.py
+++ markdown_generator/pubsFromBib.py
@@ -163,7 +163,7 @@ for pubsource in publist:
                     md += "\nexcerpt: '" + html_escape(b["note"]) + "'"
                     note = True
 
-            md += "\ndate: " + str(pub_year) 
+            md += "\ndate: " + str(pub_date) 
 
             md += "\nvenue: '" + html_escape(venue) + "'"
             
